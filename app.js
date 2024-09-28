738f40bff41e5664438ded85cadf7452526b6314
Ayanaanonaeem <ayanaano5@gmail.com>
2024-09-29 03:31:07 +0500

    file create

---
+++ app.js
@@ -0,0 +1,34 @@
+// var cont=document.getElementById("hij")
+// var contact_html=document.getElementById("contact_html")  
+// var index_html=document.getElementById("index_html")    
+// var diamond=document.getElementById("diamond")  
+// var gold=document.getElementById("gold")
+// var platinum=document.getElementById("platinum")
+// var silver=document.getElementById("silver")
+
+// cont.addEventListener("click",()=>{
+    
+    
+//     contact_html.style.display="block"
+//     index_html.style.display="none"
+    
+    
+//     var a= setTimeout(() => {
+//         diamond.style.height="0px"
+//         gold.style.display="none"
+//         platinum.style.display="none"
+//         silver.style.display="none"
+
+        
+    
+//     }, 1000);
+    
+    
+    
+    
+// })
+
+
+
+
+
